46c2a363e63eee355dae55d9b8bf32d9549ea798
Zé Gabriel <kidgbzin@gmail.com>
2022-06-01 01:17:24 -0300

    Adicionado ligação entre as entidades.
    - Corrigido a ligação entre "Categorias" e "Eventos".
    - Adicionado opção de adicionar múltiplas categorias.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,16 +27,17 @@ class HomePage extends StatefulWidget {
   HomePageState createState() => HomePageState();
 }
 
-class HomePageState extends State<HomePage> with TickerProviderStateMixin{
+class HomePageState extends State<HomePage> with TickerProviderStateMixin {
   late TabController _tabController;
   final TextEditingController eventName = TextEditingController();
   final TextEditingController eventLocal = TextEditingController();
   final TextEditingController eventCategory = TextEditingController();
   final TextEditingController categoryName = TextEditingController();
+  final List<String> cats = <String>[];
 
   final CollectionReference _events = FirebaseFirestore.instance.collection('Events');
   final CollectionReference _categories = FirebaseFirestore.instance.collection('Categories');
-  
+
   @override
   void initState() {
     super.initState();
@@ -49,55 +50,119 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin{
       action = 'update';
       eventName.text = documentSnapshot['Name'];
       eventLocal.text = documentSnapshot['Local'];
-      eventCategory.text = documentSnapshot['Category'];
+      // eventCategory.text = documentSnapshot['Category'];
     }
     await showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (BuildContext ctx) {
-        return Padding(
-          padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: eventName,
-                decoration: const InputDecoration(labelText: 'Nome'),
-              ),
-              TextField(
-                controller: eventLocal,
-                decoration: const InputDecoration(labelText: 'Local'),
-              ),
-              TextField(
-                controller: eventCategory,
-                decoration: const InputDecoration(labelText: 'Categoria'),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                child: Text(action == 'create' ? 'Create' : 'Update'),
-                onPressed: () async {
-                  final String name = eventName.text;
-                  final String local = eventLocal.text;
-                  final String category = eventCategory.text;
-                  if (action == 'create') {
-                    await _events.add({"Name": name, "Local": local, "Category": category});
-                  }
-                  if (action == 'update') {
-                    await _events.doc(documentSnapshot!.id).update({"Name": name, "Local": local, "Category": category});
-                  }
-                  eventCategory.text = '';
-                  eventName.text = '';
-                  eventLocal.text = '';
-                  Navigator.of(context).pop();
-                }
-              )
-            ],
-          ),
-        );
-      }
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          ValueNotifier<List<bool>> categoryNotifier = ValueNotifier([]);
+          return Padding(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: eventName,
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                ),
+                TextField(
+                  controller: eventLocal,
+                  decoration: const InputDecoration(labelText: 'Local'),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12.0),
+                  child: Text('Categorias'),
+                ),
+                StreamBuilder(
+                  stream: _categories.snapshots(),
+                  builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                    if (snapshot.hasData) {
+                      var length = snapshot.data.docs.length ?? 0;
+                      categoryNotifier = ValueNotifier(List.generate(length, (index) => false));
+                      return ValueListenableBuilder<List<bool>>(
+                          valueListenable: categoryNotifier,
+                          builder: (context, selectedCat, _) {
+                            if (documentSnapshot != null) {
+                              snapshot.data.docs.forEach((doc) {
+                                if (documentSnapshot['Category'].contains(doc['Name'])) {
+                                  var index =
+                                      snapshot.data.docs.indexWhere((category) => category['Name'] == doc['Name']);
+                                  categoryNotifier.value[index] = true;
+                                }
+                              });
+                            }
+                            if (documentSnapshot != null) {
+                              print(documentSnapshot['Category'].length);
+                              for (var cat in documentSnapshot['Category']) {
+                                cats.add(cat);
+                              }
+                            }
+                            return Wrap(
+                              spacing: 3,
+                              children: List.generate(
+                                snapshot.data.docs.length,
+                                (index) {
+                                  return ChoiceChip(
+                                    label: Text(
+                                      snapshot.data.docs[index]['Name'],
+                                      style: const TextStyle(fontSize: 12),
+                                    ),
+                                    padding: const EdgeInsets.all(8),
+                                    selected: selectedCat[index],
+                                    selectedColor: Theme.of(context).primaryColor,
+                                    labelStyle: selectedCat[index] ? const TextStyle(color: Colors.white) : null,
+                                    onSelected: (value) {
+                                      categoryNotifier.value = List.from(categoryNotifier.value)..[index] = value;
+                                      if (value) {
+                                        cats.add(snapshot.data.docs[index]['Name']);
+                                      } else {
+                                        cats.remove(snapshot.data.docs[index]['Name']);
+                                      }
+                                    },
+                                  );
+                                },
+                              ),
+                            );
+                          });
+                    }
+
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  child: Text(action == 'create' ? 'Create' : 'Update'),
+                  onLongPress: () => print(cats),
+                  onPressed: () async {
+                    final String name = eventName.text;
+                    final String local = eventLocal.text;
+                    final List category = cats;
+
+                    if (action == 'create') {
+                      await _events.add({"Name": name, "Local": local, "Category": cats});
+                    }
+                    if (action == 'update') {
+                      await _events
+                          .doc(documentSnapshot!.id)
+                          .update({"Name": name, "Local": local, "Category": cats});
+                    }
+                    eventCategory.text = '';
+                    eventName.text = '';
+                    eventLocal.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        }).whenComplete(
+      () {
+        eventName.clear();
+        eventLocal.clear();
+        cats.clear();
+      },
     );
   }
 
@@ -108,41 +173,41 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin{
       categoryName.text = documentSnapshot['Name'];
     }
     await showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (BuildContext ctx) {
-        return Padding(
-          padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: categoryName,
-                decoration: const InputDecoration(labelText: 'Nome'),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                child: Text(action == 'create' ? 'Create' : 'Update'),
-                onPressed: () async {
-                  final String name = categoryName.text;
-                  if (action == 'create') {
-                    await _categories.add({"Name": name});
-                  }
-                  if (action == 'update') {
-                    await _categories.doc(documentSnapshot!.id).update({"Name": name});
-                  }
-                  categoryName.text = '';
-                  Navigator.of(context).pop();
-                }
-              )
-            ],
-          ),
-        );
-      }
-    );
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: categoryName,
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text(action == 'create' ? 'Create' : 'Update'),
+                  onPressed: () async {
+                    final String name = categoryName.text;
+                    if (action == 'create') {
+                      await _categories.add({"Name": name}).whenComplete(() => Navigator.of(context).pop());
+                    }
+                    if (action == 'update') {
+                      await _categories
+                          .doc(documentSnapshot!.id)
+                          .update({"Name": name}).whenComplete(() => Navigator.of(context).pop());
+                    }
+                    categoryName.text = '';
+                  },
+                )
+              ],
+            ),
+          );
+        });
   }
 
   Future<void> _deleteProduct(String productId) async {
@@ -203,8 +268,10 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin{
                           width: 100,
                           child: Row(
                             children: [
-                              IconButton(icon: const Icon(Icons.edit), onPressed: () => _createOrUpdate(documentSnapshot)),
-                              IconButton(icon: const Icon(Icons.delete), onPressed: () => _deleteProduct(documentSnapshot.id)),
+                              IconButton(
+                                  icon: const Icon(Icons.edit), onPressed: () => _createOrUpdate(documentSnapshot)),
+                              IconButton(
+                                  icon: const Icon(Icons.delete), onPressed: () => _deleteProduct(documentSnapshot.id)),
                             ],
                           ),
                         ),
@@ -238,8 +305,12 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin{
                           width: 100,
                           child: Row(
                             children: [
-                              IconButton(icon: const Icon(Icons.edit), onPressed: () => _createOrUpdateCategory(documentSnapshot)),
-                              IconButton(icon: const Icon(Icons.delete), onPressed: () => _deleteCategory(documentSnapshot.id)),
+                              IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () => _createOrUpdateCategory(documentSnapshot)),
+                              IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () => _deleteCategory(documentSnapshot.id)),
                             ],
                           ),
                         ),
@@ -259,8 +330,7 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin{
         onPressed: () {
           if (_tabController.index == 0) {
             _createOrUpdate();
-          }
-          else {
+          } else {
             _createOrUpdateCategory();
           }
         },
